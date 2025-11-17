de1051ef5e45967f46bc50f32fef5d6e72f8ab55
Rachit Mukherjee <rachitmukherjee99@gmail.com>
2025-11-17 18:32:26 +0530

    Add session info to debug endpoint

--- pages/api/debug-credentials.ts
+++ pages/api/debug-credentials.ts
@@ -1,14 +1,25 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { getSessionFromRequest } from '../../lib/auth-helpers';
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  const session = await getSessionFromRequest(req);
+  
   return res.status(200).json({
     apiKey: process.env.SHOPIFY_API_KEY,
     apiKeyFirstChars: process.env.SHOPIFY_API_KEY?.substring(0, 8),
     hasSecret: !!process.env.SHOPIFY_API_SECRET,
     host: process.env.HOST,
     nodeEnv: process.env.NODE_ENV,
+    session: session ? {
+      id: session.id,
+      shop: session.shop,
+      isOnline: session.isOnline,
+      hasAccessToken: !!session.accessToken,
+      accessTokenFirstChars: session.accessToken?.substring(0, 10),
+      scope: session.scope,
+    } : null,
   });
 }
