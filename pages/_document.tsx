6e492fd5a3ac43b2c6ea06ec55a80eb6045cd83d
Rachit Mukherjee <rachitmukherjee99@gmail.com>
2025-11-28 13:32:54 +0530

    Use data-api-key attribute for App Bridge CDN

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,19 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
+  const apiKey = process.env.NEXT_PUBLIC_SHOPIFY_API_KEY || '';
+  
   return (
     <Html lang="en">
       <Head>
-        {/* App Bridge MUST be the first script, no async/defer/module */}
-        {/* eslint-disable-next-line @next/next/no-sync-scripts */}
-        <script src="https://cdn.shopify.com/shopifycloud/app-bridge.js"></script>
         <meta charSet="utf-8" />
+        {/* App Bridge MUST be the first script, no async/defer/module */}
+        {/* API key is passed via data-api-key attribute */}
+        {/* eslint-disable-next-line @next/next/no-sync-scripts */}
+        <script 
+          src="https://cdn.shopify.com/shopifycloud/app-bridge.js"
+          data-api-key={apiKey}
+        />
         <link
           rel="stylesheet"
           href="https://unpkg.com/@shopify/polaris@12.0.0/build/esm/styles.css"
