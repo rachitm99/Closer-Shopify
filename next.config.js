7b25d61b1b3f8d18ea61cd73b4d4bbf973b232a5
Rachit Mukherjee <rachitmukherjee99@gmail.com>
2026-01-07 12:50:29 +0530

    save

--- next.config.js
+++ next.config.js
@@ -2,6 +2,63 @@
 const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ['@shopify/polaris'],
+  // Optimize bundle size
+  swcMinify: true,
+  compress: true,
+  // Reduce build output size
+  productionBrowserSourceMaps: false,
+  // Optimize images
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    deviceSizes: [640, 750, 828, 1080, 1200],
+    minimumCacheTTL: 60,
+  },
+  // Optimize chunks
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // Optimize client-side bundle splitting
+      config.optimization = {
+        ...config.optimization,
+        splitChunks: {
+          chunks: 'all',
+          cacheGroups: {
+            default: false,
+            vendors: false,
+            // Shopify packages in separate chunk
+            shopify: {
+              name: 'shopify',
+              test: /[\\/]node_modules[\\/](@shopify)[\\/]/,
+              priority: 10,
+              reuseExistingChunk: true,
+            },
+            // Polaris separately to enable better caching
+            polaris: {
+              name: 'polaris',
+              test: /[\\/]node_modules[\\/](@shopify[\\/]polaris)[\\/]/,
+              priority: 20,
+              reuseExistingChunk: true,
+            },
+            // Firebase separately
+            firebase: {
+              name: 'firebase',
+              test: /[\\/]node_modules[\\/](firebase)[\\/]/,
+              priority: 15,
+              reuseExistingChunk: true,
+            },
+            // Common vendor libraries
+            lib: {
+              test: /[\\/]node_modules[\\/]/,
+              name: 'lib',
+              priority: 5,
+              minChunks: 2,
+              reuseExistingChunk: true,
+            },
+          },
+        },
+      };
+    }
+    return config;
+  },
   async headers() {
     return [
       {
@@ -27,5 +84,6 @@ const nextConfig = {
     ];
   },
 };
+
 
 module.exports = nextConfig;
